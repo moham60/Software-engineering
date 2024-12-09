d22cc82bcdd1371580aa9f028460145dbf7444fb
moham60 <alkingg289@gmail.com>
2024-12-09 21:42:33 +0200

    first commit

---
+++ js/home.js
@@ -0,0 +1,42 @@
+
+let users=JSON.parse(localStorage.getItem("user"));
+    if(users){
+        users.forEach(e => {
+            if(e.logIn==="true"){
+                document.querySelector(".homeContainer").innerHTML=`
+                <h1>Welcome ${e.name}</h1>`
+            }
+        });
+    }
+    
+
+let darkMode=document.querySelector(".darkMode");
+
+
+darkMode.addEventListener("click",function(){
+    document.body.children[0].classList.toggle("dark-Mode")
+    var arr=Array.from(document.body.children[0].children)
+    console.log(arr)
+    arr.forEach(e=>{
+        if(e.classList.contains("bg-white")||e.classList.contains("bg-white")&&e.classList.contains("text-black")){
+            e.classList.remove("bg-white");
+            e.classList.remove("text-black");
+            e.classList.add("dark-Mode");
+        }
+        else{
+            e.classList.remove("dark-Mode");
+            e.classList.add("bg-white");
+            e.classList.add("text-black");
+            
+        }
+        
+    })
+    document.querySelectorAll("li a").forEach(e=>{
+        e.classList.toggle("btn-outline-dark");
+        e.classList.toggle("btn-outline-primary");
+    })
+    console.log(document.querySelector(".name").nextSibling)
+
+    
+})
+
