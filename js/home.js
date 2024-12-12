6355c5374273308bbc8f223a66bc26e7b587fb9b
moham60 <105382862+moham60@users.noreply.github.com>
2024-12-13 00:44:10 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -7,4 +7,8 @@ darkMode.addEventListener("click", function () {
     e.classList.toggle("shadow-lg");
     e.style.boxShadow = "0 0 4px #eee";
   });
+ document.querySelectorAll("ul li a").forEach(e=>{
+     e.classlist.toggole("btn-outline-dark");
+      e.classlist.toggole("btn-outline-primary");
+   )
 });
