1d34887cabe3afb64472c69b5078a814a522e193
moham60 <105382862+moham60@users.noreply.github.com>
2024-12-12 19:46:13 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,33 +1,10 @@
+let darkMode = document.querySelector(".darkMode");
 
-    
-
-let darkMode=document.querySelector(".darkMode");
-
-
-darkMode.addEventListener("click",function(){
-    document.body.children[0].classList.toggle("dark-Mode")
-    var arr=Array.from(document.body.children[0].children)
-    console.log(arr)
-    arr.forEach(e=>{
-        if(e.classList.contains("bg-white")||e.classList.contains("bg-white")&&e.classList.contains("text-black")){
-            e.classList.remove("bg-white");
-            e.classList.remove("text-black");
-            e.classList.add("dark-Mode");
-        }
-        else{
-            e.classList.remove("dark-Mode");
-            e.classList.add("bg-white");
-            e.classList.add("text-black");
-            
-        }
-        
-    })
-    document.querySelectorAll("li a").forEach(e=>{
-        e.classList.toggle("btn-outline-dark");
-        e.classList.toggle("btn-outline-primary");
-    })
-    console.log(document.querySelector(".name").nextSibling)
-
-    
-})
-
+darkMode.addEventListener("click", function () {
+  document.body.classList.toggle("dark-Mode");
+  document.querySelector("aside").classList.toggle("dark-Mode");
+  document.querySelectorAll(".cart").forEach((e) => {
+    e.classList.toggle("shadow-lg");
+    e.style.boxShadow = "0 0 4px #eee";
+  });
+});
