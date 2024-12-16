d5db6ed2567a85d5a76fd45c0e29f2062621cbec
moham60 <105382862+moham60@users.noreply.github.com>
2024-12-16 15:54:25 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -3,12 +3,18 @@ let darkMode = document.querySelector(".darkMode");
 darkMode.addEventListener("click", function () {
   document.body.classList.toggle("dark-Mode");
   document.querySelector("aside").classList.toggle("dark-Mode");
+  document.querySelectorAll("aside li a").forEach(e=>{
+    e.classList.toggle("btn-outline-dark");
+    e.classList.toggle("btn-outline-primary")
+  })
   document.querySelectorAll(".cart").forEach((e) => {
     e.classList.toggle("shadow-lg");
     e.style.boxShadow = "0 0 4px #eee";
   });
- document.querySelectorAll("ul li a").forEach(e=>{
-     e.classlist.toggole("btn-outline-dark");
-      e.classlist.toggole("btn-outline-primary");
-    });
-}
+});
+
+let logOut = document.getElementById("logOut");
+
+logOut.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
