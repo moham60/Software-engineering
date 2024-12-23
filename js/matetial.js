194b88cca5d9c5db2a900df03e6cd25b50f9aa5c
moham60 <105382862+moham60@users.noreply.github.com>
2024-12-23 16:15:50 +0200

    Add files via upload

---
+++ js/matetial.js
@@ -0,0 +1,72 @@
+let uploadMaterial = document.getElementById("uploadMaterial");
+let addMaterial = document.getElementById("addMaterial");
+let materialRow = document.querySelector(".materialRow");
+var countPng = 0,
+  countpdf = 0,
+  countppt = 0;
+
+addMaterial.addEventListener("click", function () {
+  if (uploadMaterial.value !== "") {
+    var extension = uploadMaterial.files[0].name.slice(
+      uploadMaterial.files[0].name.lastIndexOf(".") + 1
+    );
+    if (
+      extension === "pdf" ||
+      extension === "png" ||
+      extension === "ppt" ||
+      extension === "pptx"
+    ) {
+      materialRow.innerHTML += `
+          <div class="col-lg-4 col-md-6 text-center ">
+      <div class="assignment shadow-lg bg-white px-2 pt-5 pb-4  position-relative">
+    <i class="fa-solid fa-download position-absolute top-0 start-0 ms-2 mt-1"></i>
+    <img width="55p" src="./images/${
+      extension === "pptx" ? "eps" : extension
+    }.svg" alt="">           
+      <span class="d-block mt-2 num-assignment">${
+        uploadMaterial.files[0].name
+      } </span>
+      <div class="info mt-2 border-top pt-3 d-flex align-items-center justify-content-between   ">
+          
+      <span class="text-secondary">${(
+        uploadMaterial.files[0].size / 1000000
+      ).toPrecision(2)}mb</span>
+      </div>
+          </div>
+          </div>`;
+    } else {
+      alert("please enter valid ppt or png or pdf files");
+    }
+
+    if (extension === "png") {
+      countPng++;
+      document.querySelector(".images-files").classList.remove("d-none");
+      document.querySelector(
+        ".images-files"
+      ).innerHTML = `<div class="d-flex w-100 align-items-center justify-content-between">
+                    <span><i class="fa-regular fa-images fa-lg center-flex c-green icon text-success me-2"></i>png files</span>
+                    <span>${countPng} files</span>
+                </div>`;
+    } else if (extension === "ppt" || extension === "pptx") {
+      icon = `<i class="fa-regular fa-file-powerpoint text-warningme-2"></i>`;
+      countppt++;
+      document.querySelector(".pdf-powerPoint").classList.remove("d-none");
+      document.querySelector(
+        ".pdf-powerPoint"
+      ).innerHTML = `<div class="d-flex w-100 align-items-center justify-content-between">
+          <span><i class="fa-regular fa-file-powerpoint me-2 text-warning"></i>png files</span>
+          <span>${countppt} files</span>
+      </div>`;
+    } else if (extension === "pdf") {
+      countpdf++;
+      document.querySelector(".pdf-files").classList.remove("d-none");
+      document.querySelector(
+        ".pdf-files"
+      ).innerHTML = `<div class="d-flex w-100 align-items-center justify-content-between">
+          <span><i class="fa-regular fa-file-pdf fa-lg center-flex c-blue icon me-2 p-2 text-dangerme-2"></i>png files</span>
+
+          <span>${countpdf} files</span>
+         </div>`;
+    }
+  }
+});
