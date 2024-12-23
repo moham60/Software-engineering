7cd045d75759b3575ef9ca941575c2935839a01b
moham60 <105382862+moham60@users.noreply.github.com>
2024-12-23 23:03:56 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,16 +1,34 @@
 let darkMode = document.querySelector(".darkMode");
 
+if (JSON.parse(localStorage.getItem("darkMode")) == "true") {
+  document.body.classList.toggle("dark-Mode");
+  document.querySelector("aside").classList.toggle("dark-Mode");
+  document.querySelectorAll("aside li a").forEach((e) => {
+    e.classList.toggle("btn-outline-dark");
+    e.classList.toggle("btn-outline-primary");
+  });
+  document.querySelectorAll(".cart").forEach((e) => {
+    e.classList.add("shadow-lg");
+    e.style.boxShadow = "0 0 4px #eee";
+  });
+}
+
 darkMode.addEventListener("click", function () {
   document.body.classList.toggle("dark-Mode");
   document.querySelector("aside").classList.toggle("dark-Mode");
-  document.querySelectorAll("aside li a").forEach(e=>{
+  document.querySelectorAll("aside li a").forEach((e) => {
     e.classList.toggle("btn-outline-dark");
-    e.classList.toggle("btn-outline-primary")
-  })
+    e.classList.toggle("btn-outline-primary");
+  });
   document.querySelectorAll(".cart").forEach((e) => {
     e.classList.toggle("shadow-lg");
     e.style.boxShadow = "0 0 4px #eee";
   });
+  if (localStorage.getItem("darkMode")) {
+    localStorage.clear();
+  } else {
+    localStorage.setItem("darkMode", JSON.stringify("true"));
+  }
 });
 
 let logOut = document.getElementById("logOut");
@@ -18,14 +36,6 @@ let logOut = document.getElementById("logOut");
 logOut.addEventListener("click", function () {
   window.location.href = "index.html";
 });
-
-let saveDeleteEmail=document.getElementById('saveDeleteEmail');
-
-saveDeleteEmail.addEventListener("click", function () {
-  window.location.href = "index.html";
-});
-
-
 
 let links = document.querySelectorAll("li a");
 
@@ -37,15 +47,3 @@ links.forEach((e) => {
     e.classList.add("active");
   }
 });
-
-let forms=document.querySelectorAll('form');
-
-forms.forEach(e=>{
-  e.addEventListener("click", function (event) {
-   event.preventDefault();
-});
-});
-
-
-
-
